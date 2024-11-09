8157719766cf2533c828ba6a69a44d355b345039
Retr0G51 <brunodiaz.melody@gmail.com>
2024-11-08 21:56:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,16 @@
              { id: 8, name: "Smart TV Milexus 50", price: 450, category: "electrodomestico", image: "images/85634ced-c3d4-4452-95d1-bdf12ef02570.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
              { id: 9, name: "Smart TV ROYAL 43 ", price: 365, category: "electrodomestico", image: "images/30db0ce4-b5ce-4035-83a2-9313efd6d511.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
              { id: 10, name: "Smart TV Marca TCL FHD TCL 43", price: 370, category: "electrodomestico", image: "images/b1e00a04-fa3a-4c4b-98b6-e8f0f3c4ff52.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
-             { id: 11, name: "Smart Tv Emerald 43", price: 360, category: "electrodomestico", image: "images/c249154f-d68c-4b98-96e9-17ad5265c37e.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
+             { id: 11, name: "Smart Tv Emerald 43", price: 360, category: "electrodomestico", image: "images/c249154f-d68c-4b98-96e9-17ad5265c37e.jpg", description: "-Factura Impresa y Garantía: 1-3 meses -Transporte Incluido" }
              { id: 12, name: "Smart TV Premier 43", price: 365, category: "electrodomestico", image: "images/92d1a59b-5f65-43fd-8290-d5f6c063dad6.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
-             { id: 13, name: "Smart TV MILEXUS de 42", price: 360, category: "electrodomestico", image: "images/bafe4373-3991-437d-a2cd-311eceeca148.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
-             { id: 14, name: "Smart Tv Philips 40", price: 370, category: "electrodomestico", image: "images/7ae5f265-5c10-4964-a87e-e62473edab03.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
-             { id: 15, name: "Smart TV Premier 32", price: 250, category: "electrodomestico", image: "images/00b83b9b-8aeb-4a88-8eb2-d2237465acae.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
+             { id: 13, name: "Smart TV MILEXUS de 42", price: 360, category: "electrodomestico", image: "images/bafe4373-3991-437d-a2cd-311eceeca148.jpg", description: "-Factura Impresa y Garantía: 3 mes -Transporte Incluido" }
+             { id: 14, name: "Smart Tv Philips 40", price: 370, category: "electrodomestico", image: "images/7ae5f265-5c10-4964-a87e-e62473edab03.jpg", description: "-Factura Impresa y Garantía: 15 días -Transporte Incluido -3 puertos HDMI, 1 puerto USB y entrada analogica" }
+             { id: 15, name: "Smart TV Premier 32", price: 250, category: "electrodomestico", image: "images/00b83b9b-8aeb-4a88-8eb2-d2237465acae.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido -Android 14 -Soporte Incluido, 2 Mandos " }
              { id: 16, name: "Smart TV  HD Insignia 32", price: 250, category: "electrodomestico", image: "images/34b033b8-7c49-4a58-a1e2-589a9747bb5d.jpg", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
-             
-             
+             { id: 17, name: "Smart Tv Oska 32", price: 250, category: "electrodomestico", image: "", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
+             { id: 18, name: "Smart TV Milexus 32", price: 245, category: "electrodomestico", image: "", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
+             { id: 19, name: "Smart Tv Challenger 32", price: 265, category: "electrodomestico", image: "", description: "-Factura Impresa y Garantía: 1 mes -Transporte Incluido" }
+             { id: 20, name: "Refrigerador Premier 7 pies", price: 560, category: "electrodomestico", image: "", description: "-Factura Impresa y Garantía: 6 meses -Transporte Incluido -Congelacion Seca" }
         ];
 
         let cart = [];
